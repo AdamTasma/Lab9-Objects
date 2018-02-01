43db169ca5b5c91fb011bc0086c4f9849f03caf4
AdamTasma <adamtasma@gmail.com>
2018-02-01 15:10:38 -0500

    first commit

---
+++ Lab9/Circle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    //CLASS//
+    class Circle
+    {
+        //INSTANCE VARIABLES//
+        private double radius;
+        private const double pi = Math.PI;
+        
+        //CONSTRUCTOR//
+        public Circle(double r)
+        {
+            radius = r;
+        }
+        //METHOD//
+        public double CalculateCircumference()
+        {
+            double circumference = 2 * pi * radius;
+            return circumference;
+        }
+
+        public string CalculateFormattedCircumference()
+        {
+            double c = CalculateCircumference();
+            c = Math.Round(c, 2);
+            return c.ToString();
+        }
+
+        public double CalculateArea()
+        {
+            double area = pi * radius * radius;
+            return area;
+        }
+
+        public string CalculateFormattedArea()
+        {
+            double a = CalculateArea();
+            a = Math.Round(a, 2);
+            return a.ToString();
+        }
+    }
+}
---
+++ Lab9/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    class Program
+    {
+        public static int i = 1;
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the circle calculator, please enter a number for your radius.");
+            if (Double.TryParse(Console.ReadLine(), out double input) && input > 0)
+            {
+                //OBJECT CREATION//
+                Circle c = new Circle(input);
+                Console.WriteLine("The circumference of a circle with a radius of " + input + " is " + c.CalculateFormattedCircumference() + ".");
+                Console.WriteLine("The area of a cirlce with a radius of " + input + " is " + c.CalculateFormattedArea() + ".");
+                Repeat();
+            }
+            else
+            {
+                Console.WriteLine("I'm sorry, that wasn't a calculable number.");
+                Main();
+            }
+        }
+
+        static void Repeat()
+        {
+            Console.WriteLine("Would you like to repeat (y/n)");
+            if (char.TryParse(Console.ReadLine(), out char response))
+            {
+                if (char.ToLower(response) == 'y')
+                {
+                    i++;
+                    Main();
+                }
+                else if (char.ToLower(response) == 'n')
+                {
+                    Console.WriteLine(i + " was enough circles, eh?");
+                }
+                else
+                {
+                    Console.WriteLine("I'm sorry, I didn't understand. Would you like to continue?(y/n)");
+                    Repeat();
+                }
+            }
+            else
+            {
+                Console.WriteLine("I'm sorry, I didn't understand. Would you like to continue?(y/n)");
+                Repeat();
+            }
+        }
+    }
+
+
+}
